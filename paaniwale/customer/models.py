237d4d6768beef582ea6eb9d380afa4f7ac7f49c
imvivek71 <vivekgoswami71@gmail.com>
2019-02-16 18:27:40 +0530

    Template Added

--- paaniwale/customer/models.py
+++ paaniwale/customer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    FirstName = models.CharField(max_length=15)
+    MiddleName = models.CharField(max_length=15)
+    LastName = models.CharField(max_length=15)
+    Age = models.CharField(max_length=15)
+    Gender = models.CharField(max_length=15)
--- paaniwale/customer/views.py
+++ paaniwale/customer/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.template import loader
-
+from.models import Post
 # Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'paaniwale/base.html')
+    if request.method=='POST':
+        if request.POST.get('FirstName') and request.POST.get('MiddleName') and request.POST.get('LastName') and request.POST.get('Age') and request.POST.get('Gender'):
+            post = Post()
+            post.FirstName = request.POST.get('FirstName')
+            post.MiddleName = request.POST.get('MiddleName')
+            post.LastName= request.POST.get('LastName')
+            post.Age = request.POST.get('Age')
+            post.Gender = request.POST.get('Gender')
+            post.save()
+            return render(request, 'paaniwale/base.html')
+    else:
+        return render(request, 'paaniwale/base.html')
+
+
 def details(request):
     return render(request,'paaniwale/details.html')
